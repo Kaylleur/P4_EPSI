2f6b39acbfd507a0ae85fd63ebb0fca1ff51fb02
Shocky <a.fouquet85@gmail.com>
2015-05-20 13:44:18 +0200

    Start IHM  - créations des views  - créations des activités  - ajout des activités dans le manifest
     -test compile + lien OK

---
+++ src/com/epsi/puissance4/activities/MultyMode.java
@@ -0,0 +1,33 @@
+package com.epsi.puissance4.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.epsi.puissance4.R;
+
+public class MultyMode extends Activity implements View.OnClickListener{
+
+    private Button btnSurrend;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single);
+        btnSurrend = (Button) findViewById(R.id.btnSurrend);
+        btnSurrend.setOnClickListener(this);
+
+    }
+    public void onClick(View v) {
+        if (v == btnSurrend) {
+            Intent intention = new Intent(this,Main.class);
+            startActivity(intention);
+        }
+    }
+}
+
+
